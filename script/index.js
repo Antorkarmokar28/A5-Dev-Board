e896bc165ddc4f57d97bb8b7576f298fdb9b1a9e
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-11-24 14:47:27 +0600

    Dynamic date set in dome successfully

--- script/index.js
+++ script/index.js
@@ -1,10 +1,19 @@
 document.getElementById("QNA-box").addEventListener("click", function () {
   window.location.href = "./main.html";
 });
-
+// random background color changed
 document
   .getElementById("change-bg-color-btn")
   .addEventListener("click", function () {
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     document.getElementById("body").style.backgroundColor = randomColor;
   });
+// show dynamic date
+function updateDate() {
+  const today = new Date();
+  const formatted = today.toDateString();
+  document.getElementById("show-date").innerText = formatted;
+}
+updateDate();
+
+setInterval(updateDate, 1000);
