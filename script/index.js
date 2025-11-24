0087523ce45f66b33ee21b516c829982aab4a927
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-11-24 21:24:21 +0600

    Task added functionality done

--- script/index.js
+++ script/index.js
@@ -17,3 +17,29 @@ function updateDate() {
 updateDate();
 
 setInterval(updateDate, 1000);
+// clicked task button
+// getTaskButton("task-button-1");
+// getTaskButton("task-button-2");
+// getTaskButton("task-button-3");
+// getTaskButton("task-button-4");
+// getTaskButton("task-button-5");
+// getTaskButton("task-button-6");
+
+document.getElementById("task-button-1").addEventListener("click", function () {
+  document.getElementById("task-button-1").setAttribute("disabled", true);
+  const taskTitle = document.getElementById("task-title").innerText;
+  const taskDescription = document.getElementById("task-description").innerText;
+  const taskNumber = document.getElementById("task-number").innerText;
+  const convertedTaskNumber = parseInt(taskNumber);
+  const number = convertedTaskNumber - 1;
+  document.getElementById("task-number").innerText = number;
+  const taskCount = document.getElementById("task-count").innerText;
+  const convetredTaskCount = parseInt(taskCount);
+  const totalTaskCount = convetredTaskCount + 1;
+  document.getElementById("task-count").innerText = totalTaskCount;
+  const taskContainer = document.getElementById("task-container");
+  const p = document.createElement("p");
+  p.classList.add("text-xl", "font-bold", "mt-6");
+  p.innerText = `${taskTitle} ${taskDescription}`;
+  taskContainer.appendChild(p);
+});
---
+++ utilities/utilities.js
@@ -0,0 +1,5 @@
+// function getTaskButton(id) {
+//   document.getElementById(id).addEventListener("click", function () {
+//     console.log("clicked");
+//   });
+// }
